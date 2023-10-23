d2ee55f279101682c90b0fdc8c1942af2031396d
Tu <tun42092@temple.edu>
2023-10-23 09:53:28 -0400

    finished task 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
+const repeatString = function(word, count) {
+
+    if (count < 0){
+        return "ERROR";
+    }
+
+    let string = ""
+
+    for(let i = 0; i < count; i++){
+        string = string.concat(word)
+
+    
+    }
+    return string
 
 };
+
+repeatString("hey",3)
 
 // Do not edit below this line
 module.exports = repeatString;
